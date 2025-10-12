cd7d64c7482ef52bfc119e376172218a009eadf6
ordervschaos <anzal.ansari@gmail.com>
2025-10-12 13:53:50 -0400

    small done button and edit buttn

--- src/components/page/PageNavigation.tsx
+++ src/components/page/PageNavigation.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
-import { ArrowRight, Plus, Undo2, ArrowLeft, TableOfContents, List, Check } from "lucide-react";
+import { ArrowRight, Plus, Undo2, ArrowLeft, TableOfContents, List, Check, Pencil } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { useSidebar } from "@/components/ui/sidebar";
 import { PreloadLink } from "./PreloadLink";
@@ -54,15 +54,34 @@ export const PageNavigation = ({
 
   return (
     <div className="flex max-w-screen-lg items-center justify-center gap-4 flex-col my-4">
-      <div className="flex items-center gap-2 justify-center">
-        <Button
-          variant="ghost"
-          onClick={toggleSidebar}
-          className="flex items-center gap-2 px-4 py-4 rounded-full bg-background border-border max-w-[80vw]"
-        >
-          <span className="text-sm text-muted-foreground">Page {displayCurrentIndex}/{displayTotalPages}</span>
-        </Button>
-        
+      <div className="flex flex-row items-center gap-2 justify-center w-full">
+        <div className="flex items-center gap-2 justify-center">
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={toggleSidebar}
+            className="flex items-center gap-1 px-2 py-0.5 h-6 rounded-full bg-background border-border max-w-[80vw] text-[10px]"
+          >
+            <span className="text-muted-foreground">Page {displayCurrentIndex}/{displayTotalPages}</span>
+          </Button>
+          
+        </div>
+
+        {canEdit && (
+          isEditing ? (
+            <div className="flex items-center gap-2 justify-center">
+              <Button variant="default" size="sm" className="rounded-full" onClick={() => setIsEditing(false)}>
+                <Check className="h-4 w-4 flex-shrink-0" /> Done
+              </Button>
+            </div>
+          ):(
+            <div className="flex items-center text-muted-foreground text-xs gap-2 justify-center">
+              <Button variant="ghost" size="sm" className="rounded-full h-6 px-2 text-[10px]" onClick={() => setIsEditing(true)}>
+                <Pencil className="h-2.5 w-2.5 flex-shrink-0" /> Edit
+              </Button>
+            </div>  
+          )
+        )}
       </div>
 
       {!isEditing && (
@@ -123,15 +142,7 @@ export const PageNavigation = ({
       </div>
       )}
 
-      {
-        isEditing && (
-          <div className="flex items-center gap-2 justify-center">
-            <Button variant="default" className="rounded-full" onClick={() => setIsEditing(false)}>
-              <Check className="h-5 w-5 flex-shrink-0" /> I'm done editing
-            </Button>
-          </div>
-        )
-      }
+  
      
     </div>
   );
